2461a901b06d360fa16d23495781d5e76720506d
swanujoshi <joshiswanand20@gmail.com>
2025-08-02 23:24:37 +0530

    First commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,24 @@
+//  const is used to  declare the constant which cannot be change 
+ const account_Id =  1234
+// let is use to  declare simply which change be over written.Latest variable will print
+ let account_email = "swanandjoshi20@gmail.com"
+// var is also same like let but there are some issue in var that let has overriden 
+ var account_password = "12345"
+
+ account_city = "Pune"
+
+ let account_State;
+
+// account_Id= 2 // Not allowed
+account_email = "Swarajjoshi@gmail.com"
+account_password ="1212121"
+account_city = "Mumbai"
+ console.log(account_Id);
+ console.log([account_Id ,account_email ,account_password ,account_city,account_State]);
+ 
+ /*
+ 
+ Prefer not to use var 
+ because of issue in block scope and functional scope
+ 
+ */ 
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let name = "Swanand"  // String
+let age = 22  //Integer
+let isLoggedIn = false //Boolean
+null //standalone value  
+
+//typeof = retrun the type the varible
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object 
+console.log(typeof "Swanand")
