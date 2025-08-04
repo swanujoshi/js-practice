2d17b09b9d63eaacbc69a16d702bdf78ec76a46b
swanujoshi <joshiswanand20@gmail.com>
2025-08-04 07:08:11 +0530

    strings

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -8,3 +8,29 @@ null //standalone value
 console.log(typeof undefined); // undefined 
 console.log(typeof null); // object 
 console.log(typeof "Swanand")
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+// stack (primitive) , Heap (Non primitive)
+// Primitive  - In primitive data types the copy of and memory is created so when we update any memory the copy gets updated,
+let myYoutubename = "Carry minati"
+let anothername = myYoutubename
+anothername = "chai aurjavascript"
+console.log(anothername);
+console.log(myYoutubename);
+
+// Non-primitive - In non primitive data types the actual memory is updated.
+
+let userone = {
+
+    email : "swanand20@gmail.com",
+    upi : "121223@ybl"
+}
+
+let usertwo = userone
+
+usertwo.email ="swaraj28@gmail.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
+
