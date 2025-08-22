ab6d9e0eeb7689d36c128bf09e86df41f21e503e
swanujoshi <joshiswanand20@gmail.com>
2025-08-22 11:47:24 +0530

    Maps & For..of ,For..in

---
+++ 05_iterations/maps.js
@@ -0,0 +1,29 @@
+// For of 
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    
+    // console.log(num);
+    
+}
+
+const greeings = "Swanand"
+for (const greet of greeings) {
+    // console.log(`Each char is ${greet}`);
+}
+
+// Maps
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United states")
+map.set('FR',"France")
+map.set('IN',"India")
+
+// console.log(map);
+
+for (const [key,value] of map) {
+    // console.log(key);
+    // console.log(key , '-' , value);
+      
+}
