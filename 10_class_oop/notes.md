afbbc459b64de6c6bec1d3497591f75ad7c4fb8d
swanujoshi <joshiswanand20@gmail.com>
2025-09-06 13:08:38 +0530

    oop

---
+++ 10_class_oop/notes.md
@@ -0,0 +1,24 @@
+# Javascript and classes 
+
+## OOP
+
+## Object 
+collection of properties and methods 
+toLowerCase
+
+## Why to use OOP
+
+## Parts of OOP
+
+object literal
+- constructor Fuctions
+- prototypes 
+- classes 
+- Instances (new,this)
+
+## 4 pillars 
+
+- Abstarction 
+- Encapsulation 
+- Inheritance 
+- Polymorphism
---
+++ 10_class_oop/oop.js
@@ -0,0 +1,35 @@
+const user = {
+    username : "swanand",
+    loginCount : 8,
+    singedIn: true,
+
+    getUserDetails: function(){
+        // console.log("Got user details from database ");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails);
+// console.log(this);
+
+function User(username, loginCount ,isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn  = isLoggedIn;
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);  
+    }
+
+    return this
+}
+
+const userOne = new User("swanand", 12 ,true)
+const userTwo = new User("javascript", 11 , false)
+console.log(userOne);
+
+
+
